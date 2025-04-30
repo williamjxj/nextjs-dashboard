ea1653ef2240b99815f4e231b22c4150a5dbc01e
jxjwilliam <jxjwilliam@sina.com>
2025-04-30 21:38:55 +0800

    nextjs tutorial dashbaord

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Page() {
+    return (
+        <div className="flex flex-col gap-4">
+            <h1 className="text-2xl font-bold">Customers</h1>
+            <p>Customers Page</p>
+        </div>
+    );
+}
